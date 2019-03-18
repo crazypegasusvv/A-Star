e5a23d6b920bc0ee5332cea26c95b5da5a7459ce
virinchi <virinchivarmavannam@gmail.com>
2019-03-18 22:22:40 +0530

    Update Astar.cpp

--- Astar.cpp
+++ Astar.cpp
@@ -1,4 +1,9 @@
-#include "star_helper.h" //header file for helper functions like input and initialising data structures
+/*
+	This is a cpp program for implementing A* search algorithm based on 
+	psuedocode available in wikipedia .See more at https://en.wikipedia.org/wiki/A*_search_algorithm
+*/
+#include "star_helper.h"		 //header file for helper functions like input and initialising data structures
+
 
 /*this function prints the path from destination to source */
 void construct_path(vector<int> cameFrom,int curIndex)
@@ -13,6 +18,7 @@ void construct_path(vector<int> cameFrom,int curIndex)
 	cout<<endl;
 }
 
+
 /* the search function*/
 void AStar(int src,int dest,vector< list<int> > G,struct node plane[],int num)
 {
@@ -26,25 +32,25 @@ void AStar(int src,int dest,vector< list<int> > G,struct node plane[],int num)
 	my_pq(open_list);   
   
   
-	plane[src].op=true;         //marking source node as in open list
+	plane[src].op=true;         			//marking source node as in open list
   
   
-	plane[src].g=0;             //distance from source to itself is zero
+	plane[src].g=0;           			//distance from source to itself is zero
   
   
-	open_list.push(plane[src]); //adding source destination to the open list
+	open_list.push(plane[src]); 			//adding source destination to the open list
   
-	vector<int> cameFrom;       //vector for storing best predecessor in the path
-	cameFrom.resize(num,INF);   //initializing vector with size=no. of nodes
+	vector<int> cameFrom;       			//vector for storing best predecessor in the path
+	cameFrom.resize(num,INF);   			//initializing vector with size=no. of nodes
   
   /*
   
-    continue until there is no node in priority queue
-    implies exhaustion of search 
+    do until there is no node in priority queue
+    exit --->implies exhaustion of search 
     
   */
   
-  while(!open_list.empty())
+  	while(!open_list.empty())
 	{
   
     /*
@@ -69,28 +75,32 @@ void AStar(int src,int dest,vector< list<int> > G,struct node plane[],int num)
     
 		int cur_index=current.index;
     
-		plane[cur_index].op=false;        //removing node from open list
+		plane[cur_index].op=false;        					//removing node from open list
 		open_list.pop();                    
     
-		plane[cur_index].cl=true;         //adding the node to closed list
+		plane[cur_index].cl=true;         					//adding the node to closed list
 
 
-		list<int>::iterator iter;         //iterator for accessing vertices in adjacency list
+		list<int>::iterator iter;         				       //iterator for accessing vertices in adjacency list
 		for(iter=G[cur_index].begin();iter!=G[cur_index].end();iter++) {
 			int i=*iter;
-			if(plane[i].cl==true) continue;                               //if node is already in closed list (or evaluated)
-				int temp_g=current.g+distance(plane[cur_index],plane[i]);  //sum of estimate and distance
+			if(plane[i].cl==true) continue;                                  //if node is already in closed list (or evaluated)
+			int temp_g=current.g+distance(plane[cur_index],plane[i]);  	//sum of estimate and distance
         
-				if(plane[i].op==false)                                    //if node is not in open list already
-				{
-        	cameFrom[i]=cur_index;                                  //updating predecessor of node in what would be a shortest path
-					plane[i].g=temp_g;                                      //distance measure
-					plane[i].h=distance(plane[i],plane[dest]);              //heuristic
-					open_list.push(plane[i]);                               //adding node to open list
-				}
-				else if(temp_g>=plane[i].g) continue;                      //adjacent vertex is not optimal to include in shortest path
+			if(plane[i].op==false)                                   	 //if node is not in open list already
+			{
+        			cameFrom[i]=cur_index;                                 	 //updating predecessor of node in what would be a shortest path
+				plane[i].g=temp_g;                                      //distance measure
+				plane[i].h=distance(plane[i],plane[dest]);              //heuristic
+				open_list.push(plane[i]);                               //adding node to open list
+			}
+			else if(temp_g>=plane[i].g) continue;                           //adjacent vertex is not optimal to include in shortest path
 		}
-		/*for(int i=0;i<num;i++)
+	  
+	  
+		/*
+		//below is a version for representation of graph using adjacency matrix
+		for(int i=0;i<num;i++)
 		{
 			if(G[cur_index][i]==1)
 			{
@@ -106,36 +116,56 @@ void AStar(int src,int dest,vector< list<int> > G,struct node plane[],int num)
 				else if(temp_g>=plane[i].g) continue;
 			}
 		}*/
+	  
 	}  
 	cout<<"\nNo Path\n";     
 }
 
 int main()
 {
-	list<node> closed_list;
+	/*
+	
+		st-index of source vertex from nodes 
+		en-destination index
+		num-total no. of vertices
+	
+	*/
 	int st,en,num;
 	cout<<"enter no. of objects: ";
 	cin>>num;
-	node plane[num];
-	fill_nodes(plane,num);
+	node plane[num];			//nodes or vertices in the graph 
+	fill_nodes(plane,num);			//routine for taking coordinates input from file  defined in "star_helper.h"
 
-	vector< list<int> > G;
-	/*int **G;
-	G=new int*[num];
-	for(int i=0;i<num;i++) G[i]=new int[num];*/
-	init_graph(G,num);
+	
+	/*
+	
+		representation for adjacency list where the graph is directed and weighted.
+		the weight of an edge is taken as the distance between them 
+		
+	*/
+	vector< list<int> > G;	
+	
+	/*
+	this is a version for taking input as adjacency matrix
+	//int **G;
+	//G=new int*[num];
+	//for(int i=0;i<num;i++) G[i]=new int[num];
+	*/
+	
+	init_graph(G,num);						//takes the adjacenct list input 
 	
 	cout<<"choose the distance measure:\n1.Euclidean\n2.Manhattan\n";
 	int inp_d;
-	cin>>inp_d;
+	cin>>inp_d; 							//choice for euclidean or manhattan distance measure
 	if(inp_d==1) euclidean=true;
 	else if(inp_d==2) manhattan=true;
 	cout<<"enter source index: ";
 	cin>>st;
 	cout<<"enter destination index: ";
 	cin>>en;
-	AStar(st,en,G,plane,num);
+	AStar(st,en,G,plane,num);					// calling A-Star algorithm 
 	return 0;
+	
 }
 
 
